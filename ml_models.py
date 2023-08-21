b35a5b0a67ee2d47bc622987124288813467ba95
adamB6 <asbotens@gmail.com>
2023-08-21 12:15:29 -0500

    added one_vs_all

--- ml_models.py
+++ ml_models.py
@@ -3,17 +3,81 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import trend_finder as tf
+import trend_finder_lr as tflr
+import trend_finder_ovr as tfovr
+import yfinance as yf
 import pickle
 from datetime import datetime
 from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics, svm
 from imblearn.over_sampling import RandomOverSampler
 
-class svc:
+class one_vs_rest:
     def __init__(self, name):
         self.name = name
+        self.logmodel = LogisticRegression()
+        self.accuracy = 0
+        
+    def train_model(self, structured_df):
+        selected_columns = [col for col in structured_df.columns if col != 'Buy/Sell']
+        
+        X = structured_df[selected_columns]
+        
+        y = structured_df['Buy/Sell']
+        
+        # Balance the data using RandomOverSampler
+        ros = RandomOverSampler(random_state=42)
+        X_resampled, y_resampled = ros.fit_resample(X, y)
+        
+        X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.25)
+        print('test')
+        logmodel = OneVsRestClassifier(LogisticRegression(solver='lbfgs', max_iter=5000))
+        logmodel.fit(X_train, y_train)
+        y_pred = logmodel.predict(X_test)
+        
+        accuracy = metrics.accuracy_score(y_test, y_pred)
+        
+        print('The testing accuracy is: %0.2f' % accuracy)
+        #print('Coefficients:\n', logmodel.coef_)
+        self.logmodel = logmodel
+        self.accuracy = accuracy
+    
+    def save_current_model(self):
+        filename = '{}_lr_{}.sav'.format(self.name, self.accuracy)
+        pickle.dump(self.logmodel, open(filename, 'wb'))
+        print(f'Model saved to {filename}')
+        
+    def load_model(self, file):
+        self.logmodel = pickle.load(open(file, 'rb'))
+        
+    
+    def get_current_prediction(self, stock):
+        ## Get prediction at current date
+        stock = yf.Ticker(f'{stock}')
+        stock = pd.DataFrame(stock.history(period='60d'))
+        stock = stock[['Open', 'Close', 'Volume']]
+        
+        ## Pull the prices and normalize them
+        stock_prices = stock.Open.tolist() + stock.Close.tolist()
+        price_min, price_max = min(stock_prices), max(stock_prices)
+        
+        for i, val in enumerate(stock_prices):
+            stock_prices[i] = (val-price_min) / (price_max - price_min)
+            
+            
+        ## Pull the volume and normalize
+        stock_volume = stock.Volume.tolist()
+        volume_min, volume_max = min(stock_volume), max(stock_volume)
+        for i, val in enumerate(stock_volume):
+            stock_volume[i] = (val-volume_min) / (volume_max - volume_min)
+            
+        full_list = stock_prices + stock_volume
+        full_list_df = pd.DataFrame(full_list).transpose()
+        
+            
+        print(self.logmodel.predict(full_list_df))
     
 class logistic_regression:
     def __init__(self, name):
@@ -54,12 +118,10 @@ class logistic_regression:
         self.logmodel = pickle.load(open(file, 'rb'))
         
     
-    def get_current_prediction(self):
-        pass
-        '''
+    def get_current_prediction(self, stock):
         ## Get prediction at current date
-        stock = yf.Ticker('TSLA')
-        stock = pd.DataFrame(stock.history(period='20d'))
+        stock = yf.Ticker(f'{stock}')
+        stock = pd.DataFrame(stock.history(period='60d'))
         stock = stock[['Open', 'Close', 'Volume']]
         
         ## Pull the prices and normalize them
@@ -80,8 +142,7 @@ class logistic_regression:
         full_list_df = pd.DataFrame(full_list).transpose()
         
             
-        print(logmodel.predict(full_list_df))
-        '''
+        print(self.logmodel.predict(full_list_df))
 
 def main():
     tesla = tf.Historydf('TSLA', '5y', 30, 5, 60)
@@ -90,6 +151,7 @@ def main():
     tesla_lr = logistic_regression('Tesla')
     tesla_lr.train_model(tesla.structured_df)
     tesla_lr.save_current_model()
+    tesla_lr.get_current_prediction('TSLA')
 
     
     
---
+++ trend_finder_ovr.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 14:48:28 2023
+
+@author: asbot
+"""
+
+import pandas as pd
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.utils import resample
+from imblearn.under_sampling import RandomUnderSampler
+
+"""
+A df of desired stock's history and it's attributes for modification
+    Attributes:
+        name                   The name of the stock.
+        history_length       The amount of history to be pulled. i.e. '20d' for 20 days. '1y' for one year. '1m' for one month.
+        trend_length            The length of desired trend.
+        percent_change          The percentage of desired change throughout the trend. Negative values Correspond to decreases in value.
+        num_of_pretrend_days      Number of trading days that occured before the trend. These days are used for the logistic regression model parameters.
+        df                   The full dataframe of the stock's history. Only contains open and close price, and volume for each day.
+        pretrends_df         The df after searching for trends and saving the pre-trend days.
+        structured_df        The dataframe for holding normalized and restructured data.
+        balanced_df          The dataframe for holding the data after its been balanced. Can be upsampled or downsampled.
+"""
+    
+class Historydf:
+
+    def __init__(self, name, history_length, trend_length, percent_change, num_of_pretrend_days):
+        self.name = name
+        self.history_length = history_length
+        self.df = self.get_history_df(self.name)
+        self.trend_length = trend_length
+        self.percent_change = percent_change
+        self.num_of_pretrend_days = num_of_pretrend_days
+        self.pretrends_df = pd.DataFrame()
+        self.structured_df = pd.DataFrame()
+        
+        
+    # Get df of entire given stock history. df contains dfs of [open, close, volume]
+    def get_history_df(self, test):
+        stock = yf.Ticker(test)
+        stock = pd.DataFrame(stock.history(period=self.history_length))
+        stock.reset_index(drop=True)
+        stock = stock[['Open', 'Close', 'Volume']]
+        new_stock = stock.reset_index(drop=True)
+        return new_stock
+    
+    # Process df based on desired trend lengths and desired percentage of change
+    def find_trends(self):
+        new_df = pd.DataFrame(columns=['Open', 'Close', 'Volume', 'Buy/Sell'])
+        start_index = self.num_of_pretrend_days - 1
+        end_index = len(self.df) - (self.trend_length)
+        print(f"start index = {start_index}, end index = {end_index}")
+        
+        for i in range(start_index, end_index):
+            trend_percentage_change = ((self.df.iloc[i + self.trend_length, 1] - self.df.iloc[i, 1]) / self.df.iloc[i, 1]) * 100
+            if trend_percentage_change >= self.percent_change:
+                for j in range(i - (self.num_of_pretrend_days), i):
+                    # If percentage change is positive, label every set of pretrend days with "1", denoting a buy signal
+                    # This will occur at every {num_of_pretrend_days}
+                    if j == i - 1:
+                        new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                        new_row['Buy/Sell'] = 1
+                        new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+                    else:
+                       new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                       new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+            elif trend_percentage_change <= -self.percent_change:
+                for j in range(i - (self.num_of_pretrend_days), i):
+                    # If percentage change is negative, label every set of pretrend days with "0", denoting a sell signal
+                    # This will occur at every {num_of_pretrend_days}
+                    if j == i - 1:
+                        new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                        new_row['Buy/Sell'] = 0
+                        new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+                    else:
+                        new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                        new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+            else:
+                for j in range(i - (self.num_of_pretrend_days), i):
+                    # If percentage change is negative, label every set of pretrend days with "0", denoting a sell signal
+                    # This will occur at every {num_of_pretrend_days}
+                    if j == i - 1:
+                        new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                        new_row['Buy/Sell'] = 2
+                        new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+                    else:
+                        new_row = pd.DataFrame(self.df.iloc[j]).transpose()
+                        new_df = pd.concat([new_df, new_row], axis=0, ignore_index=True)
+
+        self.pretrends_df = new_df
+        
+    # Normalize the df for every desired number of pretrend days (num_of_pretrend_days)
+    # This helps to deal with the increase of stock price over time 
+    # and difference in stock prices and volumes between companies
+    def structure_data(self):
+        open_prices = self.pretrends_df[['Open']].transpose()
+        close_prices = self.pretrends_df[['Close']].transpose()
+        volume = self.pretrends_df[['Volume']].transpose()
+        buy_sell = self.pretrends_df[['Buy/Sell']]
+        buy_sell = buy_sell.dropna(axis=0)
+        open_prices = open_prices.reset_index(drop=True)
+        close_prices = close_prices.reset_index(drop=True)
+        volume = volume.reset_index(drop=True)
+        buy_sell = buy_sell.reset_index(drop=True)
+        total_columns = open_prices.shape[1]
+
+        # Initialize empty lists to store the chunks of (open, close, volume)
+        combined_chunks = []
+        normalized_prices_chunk = []
+        normalized_volume_chunk = []
+        scaler = MinMaxScaler()
+        buy_sell_counter = 0
+
+        # Iterate through the columns in chunks of {num_of_pretrend_days}
+        for i in range(0, len(self.pretrends_df), self.num_of_pretrend_days):
+            chunk_start = i
+            chunk_end = min(i + self.num_of_pretrend_days, total_columns)
+
+            # Get the chunk of columns
+            chunk_open = open_prices.iloc[:, chunk_start:chunk_end]
+            chunk_close = close_prices.iloc[:, chunk_start:chunk_end]
+            chunk_volume = volume.iloc[:, chunk_start:chunk_end]
+
+            # Concatenate the chunks along rows (axis=1) to create a single row dataframe
+            # Then normalize the open and close prices together, by chunk
+            combined_chunk = pd.concat([chunk_open, chunk_close], axis=1, ignore_index=True)
+            normalized_prices_chunk = pd.DataFrame(scaler.fit_transform(combined_chunk.T).T, columns=combined_chunk.columns)
+            normalized_volume_chunk = pd.DataFrame(scaler.fit_transform(chunk_volume.T).T, columns=chunk_volume.columns)
+
+            # Get the 'Buy/Sell' value for the current chunk
+            current_buy_sell = buy_sell.loc[buy_sell_counter, 'Buy/Sell']
+            buy_sell_counter += 1
+
+            # Append the combined chunk to the list
+            combined_chunk_with_buy_sell = pd.concat([normalized_prices_chunk, normalized_volume_chunk], axis=1, ignore_index=True)
+            combined_chunk_with_buy_sell['Buy/Sell'] = current_buy_sell
+            combined_chunks.append(combined_chunk_with_buy_sell)           
+        
+            # Concatenate the list of combined chunks along rows to create the final dataframe
+            final_combined_df = pd.concat(combined_chunks, ignore_index=True)
+        
+        # Store the structured and normalized dataframe
+        self.structured_df = final_combined_df
+        
+def main():
+    tesla = Historydf('TSLA', '1y', 5, 1, 5)
+    tesla.find_trends()
+    tesla.structure_data()
+    print(tesla.structured_df)
+    
+if __name__ == '__main__':
+    main()
